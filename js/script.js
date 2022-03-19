9db0c10fd3583094111a9db54c10c8e6025968dc
Mesquita-Dev <lmesquita2k@gmail.com>
2022-03-19 18:58:05 -0300

    javascript-perguntas

--- js/script.js
+++ js/script.js
@@ -23,3 +23,23 @@ function ativarProduto (parametro){
 }
 
 parametros.forEach(ativarProduto)
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll (".perguntas button");
+
+function ativarPergunta (event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById (controls);
+
+  resposta.classList.toggle("ativo");
+  const ativa = resposta.classList.contains("ativo");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas (pergunta){
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas)
